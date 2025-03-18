7731b52cca2badcfa0a191e3b348f0cc7b9e32ae
DavideTosti <davidetosti2002@gmail.com>
2025-03-18 17:56:28 +0100

    iniziato js

---
+++ index.js
@@ -0,0 +1,68 @@
+document.getElementById("anno").textContent = new Date().getFullYear();
+
+//aboutus
+const elementop = document.createElement("p");
+elementop.innerText = "Il Gruppo2 è una classe di Develhope";
+elementop.setAttribute("class", "elemento-creato");
+
+function aboutus() {
+  const prendi = document.getElementById("aboutus");
+
+  // Controlla se il paragrafo esiste già per non duplicarlo
+  if (!prendi.contains(elementop)) {
+    prendi.appendChild(elementop);
+    elementop.appendChild(bottone);
+  }
+}
+//bottone
+const bottone = document.createElement("button");
+bottone.innerText = "X";
+bottone.setAttribute("id", "btn");
+bottone.setAttribute("onclick", reset);
+//
+function artwork() {
+  const prendi = document.getElementById("artwork");
+  const elementop = document.createElement("p");
+  elementop.innerText = "vasffanculo";
+  prendi.appendChild(elementop);
+  elementop.setAttribute("class", "elemento-creato");
+
+  // Controlla se il paragrafo esiste già per non duplicarlo
+  if (!prendi.contains(elementop)) {
+    prendi.appendChild(elementop);
+    elementop.appendChild(bottone);
+  }
+}
+function api() {
+  const prendi = document.getElementById("api");
+  const elementop = document.createElement("p");
+  elementop.innerText = "stronzo";
+  prendi.appendChild(elementop);
+  elementop.setAttribute("class", "elemento-creato");
+
+  // Controlla se il paragrafo esiste già per non duplicarlo
+  if (!prendi.contains(elementop)) {
+    prendi.appendChild(elementop);
+    elementop.appendChild(bottone);
+  }
+}
+function licenza() {
+  const prendi = document.getElementById("licenza");
+  const elementop = document.createElement("p");
+  elementop.innerText = "cornuto";
+  prendi.appendChild(elementop);
+  elementop.setAttribute("class", "elemento-creato");
+
+  // Controlla se il paragrafo esiste già per non duplicarlo
+  if (!prendi.contains(elementop)) {
+    prendi.appendChild(elementop);
+    elementop.appendChild(bottone);
+  }
+}
+
+function reset() {
+  const prendi = document.getElementById("aboutus");
+  if (prendi.contains(elementop)) {
+    prendi.removeChild(elementop);
+  }
+}
